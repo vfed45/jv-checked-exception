0f2c6271a2f82cfc5d86887ecb9b86c7d511ba42
Viktoriia <fvee737@gmail.com>
2025-09-28 11:37:50 +0200

    fix: fixed bugs again

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,14 +5,14 @@ public class UserService {
     private final UserDao userDao = new UserDao();
 
     public void registerUser(User user) {
-            try {
-                String email = user.getEmail();
-                String password = user.getPassword();
-                String repeatPassword = user.getRepeatPassword();
-                passwordValidator.validate(password, repeatPassword);
-                userDao.saveUser(user);//write your code here
-            } catch (PasswordValidationException e) {
-                System.out.println("Your passwords are incorrect. Try again.");
-            }
+        try {
+            String email = user.getEmail();
+            String password = user.getPassword();
+            String repeatPassword = user.getRepeatPassword();
+            passwordValidator.validate(password, repeatPassword);
+            userDao.saveUser(user);//write your code here
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
+}
