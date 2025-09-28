eaac5388f6b79b4a8f0afc88c13b19fffc80a35d
Viktoriia <fvee737@gmail.com>
2025-09-28 10:21:33 +0200

    created a new class UserDao

---
+++ src/main/java/core/basesyntax/UserDao.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class UserDao {
+    public void saveUser(String email) {
+        System.out.println("User with email " + email
+        + " successfully saved to database.");
+    }
+}
